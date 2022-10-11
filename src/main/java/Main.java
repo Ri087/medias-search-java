bd5308fefaaced84ee7cae2de1e9aa4a7a0fc6ca
Ri087 <jeremydura@hotmail.fr>
2022-10-12 00:13:08 +0200

    add class get api

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
-public class Main {
+import java.io.IOException;
 
-    public static void main(String[] args) {
-        System.out.print("Hello");
+public class Main {
+    String original_title;
+    public static void main(String[] args) throws IOException {
+        getApi example = new getApi();
+        String response = example.run("https://api.themoviedb.org/3/movie/551?api_key=9c08262321f4e984577c6abaa528b6b7");
+        System.out.println(response);
     }
 
 }
+
+
+
