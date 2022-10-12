fc3a83d0ba1ac872ae549376df5be9d2d6452dcf
mb <MBARRE@martin.barre@ynov.com>
2022-10-12 21:31:23 +0200

    Menu

--- src/main/java/Api.java
+++ src/main/java/Api.java
@@ -1,3 +1,5 @@
+import com.google.gson.Gson;
+import dto.MediasDto;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -6,12 +8,14 @@ import java.io.IOException;
 
 public class Api {
     OkHttpClient client = new OkHttpClient();
-    public String getApi(String url) throws IOException {
+    public MediasDto getApi(String url) throws IOException {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
         try (Response response = client.newCall(request).execute()){
-            return response.body().string();
+            Gson gson = new Gson();
+            MediasDto medias = gson.fromJson(response.body().string(), MediasDto.class);
+            return medias;
         }
 
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import com.google.gson.Gson;
+import dto.MediasDto;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,15 +16,40 @@ public class Main {
     String original_title;
 
     public static void main(String[] args) throws IOException {
-        Scanner myObj = new Scanner(System.in);
-        Api searchApi = new Api();
 
-        System.out.println("nom: ");
-        String nameSearch = myObj.nextLine();
+        Scanner scanner = new Scanner(System.in);
 
-        String response = searchApi.getApi("https://api.themoviedb.org/3/search/multi?api_key=9c08262321f4e984577c6abaa528b6b7&query=" + nameSearch);
-        System.out.println(response);
+        System.out.println("Choose from these choices");
+        System.out.println("-------------------------\n");
+        System.out.println("1 - Rechercher un média par son nom\n");
+        System.out.println("2 - Consulter le détail d'un média");
+
+        int choice = scanner.nextInt();
+
+        while (choice < 1 || choice > 2) {
+            System.out.print("\nError! Incorrect choice.\n");
+            System.out.println(choice);
+            choice = scanner.nextInt();
+        }
+
+        switch (choice) {
+            case 1:
+                Scanner myObj = new Scanner(System.in);
+                Api searchApi = new Api();
+
+                System.out.println("nom: ");
+                String nameSearch = myObj.nextLine();
+
+                MediasDto response = searchApi.getApi("https://api.themoviedb.org/3/search/multi?api_key=9c08262321f4e984577c6abaa528b6b7&query=" + nameSearch);
+                System.out.println(response);
+                break;
+            case 2:
+                // Perform "encrypt number" case.
+                break;
+        }
+
     }
+
 }
 
 
