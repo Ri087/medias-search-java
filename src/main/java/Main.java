3c690b9588ab1425a475e65048e1806783d006e6
Ri087 <jeremydura@hotmail.fr>
2022-10-12 23:58:52 +0200

    boucle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,24 +13,29 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Choose from these choices");
-        System.out.println("-------------------------\n");
-        System.out.println("1 - Rechercher un média par son nom\n");
-        System.out.println("2 - Consulter le détail d'un média");
+        int choice = 0;
 
-        int choice = scanner.nextInt();
+        while (choice != 3) {
 
-        while (choice < 1 || choice > 2) {
-            System.out.print("\nError! Incorrect choice.\n");
-            System.out.println(choice);
+            System.out.println("\n");
+            System.out.println("Choose from these choices");
+            System.out.println("-------------------------\n");
+            System.out.println("1 - Rechercher un média par son nom\n");
+            System.out.println("2 - Consulter le détail d'un média\n");
+            System.out.println("3 - Quitter");
+
             choice = scanner.nextInt();
-        }
+
+            if (choice < 1 || choice > 3 ){
+                System.out.print("\nError! Incorrect choice.\n");
+            }
+
 
         switch (choice) {
             case 1:
                 Scanner myObj = new Scanner(System.in);
 
-                System.out.print("nom: ");
+                System.out.print("Entrer le nom de votre filme/serie: ");
                 String nameSearch = myObj.nextLine();
 
                 List<Media> media = new Api().getApi(nameSearch);
@@ -43,7 +48,12 @@ public class Main {
             case 2:
                 // Perform "encrypt number" case.
                 break;
+            case 3:
+                return;
+
+            }
         }
+
 
     }
 
